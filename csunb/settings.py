75f19d76bd357a98e24503634ee79773a8648cc4
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-10-21 10:41:42 -0200

    Let's learn how to deal with this templating system.

--- csunb/settings.py
+++ csunb/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'webappcs',
         'USER': 'ieeecs',
-        'PASSWORD': '',
+        'PASSWORD': 'test123',
         'HOST': 'localhost',
         'PORT': '',
     }
