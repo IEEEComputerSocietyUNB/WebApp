a9ad34625e499d3dc4fac9a072701e564d4a343b
alexandrebarbaruiva <lexander.santos@gmail.com>
2017-10-21 10:28:58 -0200

    First meeting online

--- blog/test_front.py
+++ blog/test_front.py
@@ -11,8 +11,9 @@ class TestSelenium(TestCase):
 
     def setUp(self):
         # create the fake browser
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, 10)
+        self.page = 'http://localhost:8000'
 
     def waitForElement(self, type, name):
         if type == 'id':
@@ -22,18 +23,23 @@ class TestSelenium(TestCase):
             return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME,
                                                             name)))
 
-    def test_pass(self):
-        pass
-
     def test_check_if_main_page_exists(self):
-        virtual_page = requests.get('http://localhost:5000')
-        self.assertEqual(virtual_page.status_code, 200)
+        status = requests.get(self.page).status_code
+        self.driver.get(self.page)
+        self.assertEqual(status, 200)
+        self.assertTrue("Computer Society" in self.driver.title)
+
+    def test_check_if_calendar_page_exists(self):
+        page = self.page + '/calendario'
+        status = requests.get(page).status_code
+        self.driver.get(page)
+        self.assertEqual(status, 200)
 
     # TODO: colocar um calendário de verdade!
-    def test_if_calendar_is_on_main_page(self):
-        self.driver.get('http://localhost:5000')
-        calendar = self.driver.find_element_by_id('calendar')
-        self.assertEqual(calendar.text, '20/09/1994')
+    # def test_if_calendar_is_on_main_page(self):
+    #     self.driver.get('http://localhost:8000')
+    #     calendar = self.driver.find_element_by_id('calendar')
+    #     self.assertEqual(calendar.text, '20/09/1994')
 
     def tearDown(self):
         # wait a little and close the fake web
