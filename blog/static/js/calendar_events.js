5ca645a8e3adc91a7b2f346277869c89fb773dfe
alexandrebarbaruiva <lexander.santos@gmail.com>
2017-11-28 11:46:30 -0200

    Create day events model

---
+++ blog/static/js/calendar_events.js
@@ -0,0 +1,6 @@
+function chama_eventos_do_dia(dia, mesano){
+    if(dia == 31){
+        var eventos_do_dia = document.getElementById("eventos-do-dia")
+        eventos_do_dia.innerHTML = "<h1>31 de Outubro</h1>"
+    }
+}
