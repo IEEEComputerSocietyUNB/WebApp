1508450a80ae684cd7ae433594c3e2599a5fb80b
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-11-30 17:33:43 -0200

    Drawing the rest of the calendar

--- blog/static/js/calendar_events.js
+++ blog/static/js/calendar_events.js
@@ -59,7 +59,7 @@ function draw_calendar(year, month) {
     // Generating weekdays for calendar
     var first_day_of_the_month = new Date(year, month);
     var first_weekday = first_day_of_the_month.getDay();
-    var number_of_days = new Date(year, month, 0).getDate();
+    var number_of_days = new Date(year, month+1, 0).getDate();
     var current_day = 1;
     var table = `<tr>
         <th>D</th>
@@ -70,8 +70,6 @@ function draw_calendar(year, month) {
         <th>S</th>
         <th>S</th>
     </tr>`
-    console.log(number_of_days);
-    console.log(first_day_of_the_month);
 
     // -- Drawing first line
     var line = "<tr>"
@@ -87,88 +85,24 @@ function draw_calendar(year, month) {
     table += line;
 
     // -- Drawing following lines
-    line = "";
+    while (current_day <= number_of_days) {
+        line = "<tr>";
+        for (i = 0; i < 7; ++i) {
+            if (current_day <= number_of_days) {
+                line += "<td>" + current_day + "</td>";
+            } else {
+                line += "<td></td>";
+            }
+            current_day++;
+        }
+        line += "</tr>";
+        table += line;
+    }
 
     // Consolidating HTML
     outlet += header;
     outlet += table;
     return outlet;
-}
-
-function generate_raw_calendar() {
-    return `<table class="calendar" id="calendar">
-        <tr>
-            <th colspan="7">
-                <span class="circle t_left">
-                    <a href="/?q=calendar {{previous_month}}">
-
-                    </a>
-                </span>
-                <span class="calendar__header">
-                    <b>{{month_year}}</b>
-                </span>
-                <span class="circle t_right">
-                    <a href="/?q=calendar {{next_month}}">
-                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
-                    </a>
-                </span>
-            </th>
-        </tr>
-        <tr>
-            <th>S</th>
-            <th>M</th>
-            <th>T</th>
-            <th>W</th>
-            <th>T</th>
-            <th>F</th>
-            <th>S</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-        </tr>
-        <tr>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-            <td>13</td>
-            <td>14</td>
-        </tr>
-        <tr>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-            <td>20</td>
-            <td>21</td>
-        </tr>
-        <tr>
-            <td>22</td>
-            <td>23</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>27</td>
-            <td>28</td>
-        </tr>
-        <tr>
-            <td>29</td>
-            <td>30</td>
-            <td><button type="button" onclick="chama_eventos_do_dia(31, '{{month_year}}')">31</button></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>`
 }
 
 var placeholder = document.getElementById('calendar_placeholder');
